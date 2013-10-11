de7ad471bef52b4b2ec46375ff7f82fdf5fd1c00
Marcus Battle <marcus@marcusbattle.com>
2013-10-11 03:04:24 -0400

    first commit

---
+++ mdb-landing.php
@@ -0,0 +1,19 @@
+<?php
+	/**
+	* Plugin Name: MDB Landing Page
+	* Description: Redirects all homepage requests to another page
+	* Version: 1.0
+	* Author: Marcus Battle
+	* Author URI: http://marcusbattle.com
+	*/
+
+	function redirect_to_home() {
+
+		if( is_home() && !isset($_REQUEST['show_home']) ) {
+			wp_redirect('theplatform');
+		}
+	}
+
+	add_action('template_redirect', 'redirect_to_home' );
+
+?>
